697d03bba72cd42e3a9cce0e7cefb4d6dcc46928
atakan erdil <atakanerdil2559@gmail.com>
2020-04-01 22:03:00 -0500

    added properties

--- Template.groovy
+++ Template.groovy
@@ -1,9 +1,4 @@
-node {
-    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([cron('* * * * *')])])
-
-
-
-
+node { 
     stage("Stage1"){ 
         echo "hello" 
 } 
