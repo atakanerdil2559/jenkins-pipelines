bc9fa476b3adb5cbe4477fbb41945e69bc9be380
atakan erdil <atakanerdil2559@gmail.com>
2020-04-08 11:36:55 -0500

    updated

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -20,7 +20,7 @@ node {
 		timestamps {
 			ws{
 				sh '''
-					aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis
+					aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 337879762040.dkr.ecr.us-east-1.amazonaws.com/artemis
 					'''
 		    }
 	    }
